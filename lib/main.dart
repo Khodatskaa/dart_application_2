93d0e90ea864c890a5e674d45e9cbac6e9666edf
Khodatskaa <khodatskaalina@gmail.com>
2024-01-25 20:02:41 +0200

    Ex 6 was done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,31 @@
-bool isEven(int number) {
-  return number % 2 == 0;
+List<int> bubbleSort(List<int> inputList) {
+  List<int> sortedList = List.from(inputList);
+
+  int n = sortedList.length;
+  bool swapped;
+
+  do {
+    swapped = false;
+
+    for (int i = 0; i < n - 1; i++) {
+      if (sortedList[i] > sortedList[i + 1]) {
+        int temp = sortedList[i];
+        sortedList[i] = sortedList[i + 1];
+        sortedList[i + 1] = temp;
+
+        swapped = true;
+      }
+    }
+
+    n--;
+  } while (swapped);
+
+  return sortedList;
 }
 
 void main() {
-  int num = 53;
-  if (isEven(num)) {
-    print("$num is even");
-  } else {
-    print("$num is odd");
-  }
+  List<int> unsortedList = [6, 1, 3, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+  List<int> sortedList = bubbleSort(unsortedList);
+  print("Unsorted List: $unsortedList");
+  print("Sorted List: $sortedList");
 }
