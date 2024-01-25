ede78f12d4277844f7b5d2a9efc881ef333e0ff0
Khodatskaa <khodatskaalina@gmail.com>
2024-01-25 19:42:48 +0200

    Ex 1 was done

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+int calculateSum(List<int> numbers) {
+  int sum = 0;
+  for (int number in numbers) {
+    sum += number;
+  }
+  return sum;
+}
+
+void main() {
+  List<int> numbers = [1, 2, 3, 4, 5];
+  int result = calculateSum(numbers);
+  print("Sum of numbers: $result");
+}
