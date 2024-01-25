8245f2f0663a961048b1034b90ec4b3e5694852e
Khodatskaa <khodatskaalina@gmail.com>
2024-01-25 22:08:24 +0200

    Ex 1 was done

---
+++ test/unit.test.dart
@@ -0,0 +1,29 @@
+import 'package:test/test.dart';
+
+int calculateSum(List<int> numbers) {
+  int sum = 0;
+  for (int number in numbers) {
+    sum += number;
+  }
+  return sum;
+}
+
+void main() {
+  group('calculateSum', () {
+    test('sum of numbers in the list', () {
+      expect(calculateSum([1, 2, 3, 4, 5]), equals(15));
+    });
+
+    test('sum of an empty list', () {
+      expect(calculateSum([]), equals(0));
+    });
+
+    test('sum of a single-item list', () {
+      expect(calculateSum([10]), equals(10));
+    });
+
+    test('sum of negative numbers', () {
+      expect(calculateSum([-1, -2, -3, -4, -5]), equals(-15));
+    });
+  });
+}
