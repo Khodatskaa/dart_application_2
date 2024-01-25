f6f8aa8ac41b5da5cdd38cb1c3c0f1243740f87b
Khodatskaa <khodatskaalina@gmail.com>
2024-01-25 19:48:41 +0200

    Ex 2 was done

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+bool isEven(int number) {
+  return number % 2 == 0;
+}
+
+void main() {
+  int num = 10;
+  if (isEven(num)) {
+    print("$num is even.");
+  } else {
+    print("$num is odd.");
+  }
+}
